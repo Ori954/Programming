fcaf7ad9cd286c2e074a346259af8f4a465f390c
Ori954 <prophet3600@gmail.com>
2018-02-23 09:27:15 +1100

    Added Unity Project

---
+++ Assets/~Astroids/scripts/moving.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    //Member valibles 
+    public float movementSpeed = 10;
+
+	// Use this for initialization
+	void Update()
+    {
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		// if the player is pressing the up arrow
+        if (input.Getkey(KeyCode.UpArrow))
+            {
+                // Move the playter up byyyyy mocement
+                transform.position.y += movmentspeed
+            }
+
+	}
+}
