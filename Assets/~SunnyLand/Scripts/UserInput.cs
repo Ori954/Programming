07955f70cc0adc57b50759d118d00db51b94b9ca
Ori954 <prophet3600@gmail.com>
2018-04-06 15:59:54 +1000

    Started implimenting PlayerController

--- Assets/~SunnyLand/Scripts/UserInput.cs
+++ Assets/~SunnyLand/Scripts/UserInput.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UserInput : MonoBehaviour {
+namespace SunnyLand
+{
+    [RequireComponent(typeof(PlayerController))]
+    public class UserInput : MonoBehaviour
+    {
+        public bool isJumping = false;
+        public bool isCrouching = false;
+        public float inputH, inputV;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        private PlayerController player;
+
+        #region Unity Functions
+        // Use this for initialization
+        void Start()
+        {
+            player = GetComponent<PlayerController>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            // Updating input
+            GetInput();
+            // Controlling the player with input
+            player.Move(inputH);
+            player.Climb(inputV);
+            if (isJumping) // If jump input has been made
+            {
+                // Tell controller to jump
+                player.Jump();
+            }
+            if (isCrouching) // If crouch input has been made
+            {
+                // Tell controller to crouch
+                player.Crouch();
+            }
+            else // Otherwise
+            {
+                // Tell controller to uncrouch
+                player.UnCrouch();
+            }
+        }
+        #endregion
+
+        #region Custom Functions
+        void GetInput()
+        {
+            inputH = Input.GetAxisRaw("Horizontal");
+            inputV = Input.GetAxisRaw("Vertical");
+            isJumping = Input.GetKeyDown(KeyCode.Space);
+            isCrouching = Input.GetKey(KeyCode.LeftControl);
+        }
+        #endregion
+    }
 }
+
